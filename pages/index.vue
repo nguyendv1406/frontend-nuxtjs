757ccba95b9249ab8523d0a6461c4de1900bce43
nguyendv1406 <nguyendv1406@gmail.com>
2023-09-05 10:22:46 +0900

    add index page

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>TEST - data: {{ data }}</div>
+</template>
+
+<script setup lang="ts">
+const { data, error } = await useFetch(`http://localhost:3010/`);
+
+console.log(data, 3333333333);
+</script>
+
+<style scoped></style>
